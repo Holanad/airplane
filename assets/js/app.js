9262a660355f65b6eaa2582fe81ca39836ba6f37
Vladislav <volkov99.at@gmail.com>
2022-08-08 16:57:08 +0300

    ewwqweqweffeeууe2eee2fffre

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,10 @@
 
 let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
+window.addEventListener('resize', () => {
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+})
+
 // Swiper slider RECALL
 const swiperStocks = new Swiper(".portfolio-slider", {
     navigation: {
